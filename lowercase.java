816bea72c2bc23f32afa2e00231f4bb5349994ba
sathishkomma <44568737+sathishkomma@users.noreply.github.com>
2018-10-31 07:41:33 -0400

    i have learnt this methods

---
+++ lowercase.java
@@ -0,0 +1,10 @@
+public class lowercase {
+        public static void main(String args[]){
+
+            String s1="abcdefghijKLMNOPqrsr";
+            String s2="ABCDEFGHIJKLmnopQRST";
+            String s1upper=s1.toUpperCase();
+            String s2lower=s2.toLowerCase();
+            System.out.println(s1upper);
+            System.out.println(s2lower);
+        }}
