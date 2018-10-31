55282defd86c6b01fb33a6525fe0c4ab48641a24
urzprashanth <44563835+urzprashanth@users.noreply.github.com>
2018-10-31 17:01:16 +0530

    Today, i have practiced this methods.

---
+++ Inttostring.java
@@ -0,0 +1,9 @@
+public class Inttostring {
+    public static void main(String[] args) {
+        int i =200;
+        int k=300;
+         String s=String.valueOf(i);
+         String s1=Integer.toString(k);
+        System.out.println(s.concat(s1));
+    }
+}
---
+++ Replace.java
@@ -0,0 +1,9 @@
+public class Replace {
+    public static void main(String[] args) {
+        String s = "prbshanth";
+        String s1 = s.replace('b','a');
+        System.out.println(s1);
+        System.out.println(s1.replace('h',' '));
+
+    }
+}
