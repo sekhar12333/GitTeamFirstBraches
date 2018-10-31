ee33316f9a675edaaa6dde8bf855ababb11c2a58
I L ABCS <pnaveenyadav2@gmail.com>
2018-10-31 16:54:56 +0530

    replace

---
+++ replace.java
@@ -0,0 +1,8 @@
+public class replace {
+    public static void main (String[]args){
+        String a="hello come to my world";
+        String b=a.replace("my","naveen");
+        System.out.println(b);
+
+    }
+}
