dda9e5d7e566e94db37b4a4fdd05d0735ab7ba12
I L ABCS <pnaveenyadav2@gmail.com>
2018-10-31 17:06:31 +0530

    empty

---
+++ empty.java
@@ -0,0 +1,9 @@
+public class empty {
+    public static void main(String[]args){
+        String naveen="";
+        String chanti="nani";
+
+        System.out.println(naveen.isEmpty());
+        System.out.println(chanti.isEmpty());
+    }
+}
