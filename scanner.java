07911220199e1e9c1046837331b3e5d45ba66705
I L ABCS <pnaveenyadav2@gmail.com>
2018-10-31 15:54:53 +0530

    happy

---
+++ scanner.java
@@ -0,0 +1,41 @@
+import java.sql.SQLOutput;
+
+import java.util.Scanner;
+
+
+
+//    public static void main(String[]args){
+//        Scanner nave=new Scanner(System.in);
+//      String a=nave.nextLine();
+//        System.out.println(a);
+
+ class apnner{
+
+
+
+     public static void main(String[]args){
+
+Scanner in=new Scanner(System.in);
+        System.out.print("input a number");
+        int num1=in.nextInt();
+
+//        System.out.println("input secound number");
+//        int num2=in.nextInt();
+//
+//        System.out.println(num1 + "*"+ num2 + "=" + (num1+ num2));
+//
+//        System.out.println(num1 + " *"+ num2 + "="+ (num1 * num2));
+//        System.out.println(num1 + "/ "+ num2 + "="+ (num1 / num2));
+//        System.out.println(num1 + "% "+ num2 + "="+ (num1 % num2));
+//
+//
+int i;
+        for( i=0; i<10; i++);{
+
+            System.out.println(num1 + "x" + (i+1) + "=" +(num1*(i+1)));
+
+
+    }
+}}
+
+
