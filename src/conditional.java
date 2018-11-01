ada622fe130317323165c38d806d7a79b7419099
butchi.vattepollu <butchi.vattepollu@gmail.com>
2018-11-01 18:10:12 +0530

    operators

---
+++ src/conditional.java
@@ -0,0 +1,12 @@
+public class conditional {
+    public static void main(String[] args) {
+
+        int a=66;
+        int b=6;
+
+
+        System.out.println(a<b && b<a );
+        System.out.println(a<b || a>b );
+
+    }
+}
---
+++ src/unary.java
@@ -0,0 +1,13 @@
+public class unary {
+    public static void main(String[] args) {
+        int a=2;
+        int b=5;
+        a++;
+        int c= b--;
+        System.out.println(c);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a != b);
+
+    }
+}
