4dbb38975b9bc0bdce83f2c6678cdff908b81340
I L ABCS <pnaveenyadav2@gmail.com>
2018-11-01 18:07:32 +0530

    opareters

---
+++ relations.java
@@ -0,0 +1,22 @@
+public class relations {
+public static void main(String[]args){
+int a=10;
+int b=20;
+String name="naveen";String name1="naveen";
+
+    System.out.println("a == b :" + (a==b));
+    System.out.println("a =< b :" + (a<=b));
+    System.out.println("a=>b :" + (a>=b));
+    System.out.println("a > b :" + (a>b));
+    System.out.println("a < b :" + (a<b));
+    System.out.println("a!=b :" + (a!=b));
+
+
+
+
+
+
+
+
+}
+}
---
+++ value.java
@@ -0,0 +1,10 @@
+public class value {
+    public static void main(String[]args){
+//       String nani="254196846416";
+//        nani=String.valueOf(nani);
+//        System.out.println(nani+658464654);
+      int nani=254196846;
+        String nani1=String.valueOf(nani);
+        System.out.println(nani1+658464654);
+    }
+    }
