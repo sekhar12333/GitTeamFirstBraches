c5198dd6c2c8481660ac0274e0db694c48707b6c
sathish <kommasathish99@gmail.com>
2018-11-20 15:28:42 +0530

    reverse

---
+++ cmline.java
@@ -0,0 +1,11 @@
+public class cmline {
+    public static void main(String[] args) {
+//        String i="sathish ";
+        char[] a=args[0].toCharArray();
+
+
+        for (int l=a.length-1;l>=0;l--){
+            System.out.print(a[l]);
+    }
+}}
+
