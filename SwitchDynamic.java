94c8115536c94b25c7ec154bc83c8b431c9abaf0
rahul98490 <1rahulganesh@gmail.com>
2018-11-20 17:40:20 +0530

    my dynamic alloation of values

---
+++ SwitchDynamic.java
@@ -0,0 +1,21 @@
+public class SwitchDynamic {
+    public static void main(String[] args) {
+
+
+        switch (args[0]){
+            case "0":
+                System.out.println("monday");
+                break;
+            case "1":
+                System.out.println("tuesday");
+                break;
+            case "2":
+                System.out.println("wednesday");
+                break;
+            case "3":
+                System.out.println("thrusday");
+                break;
+        }
+        //System.out.println(args);
+    }
+}
