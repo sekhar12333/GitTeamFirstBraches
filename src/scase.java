5889436f9ac98c6915187d0af31e03ec69e95d74
joguajith <joguajith@gmail.com>
2018-11-20 14:51:36 +0530

    today tasks

---
+++ src/scase.java
@@ -0,0 +1,28 @@
+public class scase {
+    public static void main(String[] args) {
+        switch (args[0]) {
+            case  "1":
+                System.out.println("its monday");
+               break;
+            case  "2":
+                System.out.println("its tuesday");
+                break;
+            case  "3":
+                System.out.println("its wedday");
+                break;
+            case  "4":
+                System.out.println("its thurday");
+                break;
+            case  "5":
+                System.out.println("its friday");
+                break;
+            case  "6":
+                System.out.println("its satday");
+                break;
+            case  "7":
+                System.out.println("its sunday");
+                break;
+
+        }
+    }
+}
---
+++ src/world.java
@@ -0,0 +1,13 @@
+public class world {
+    public static void main(String[] args) {
+        //String s="hello world";
+        char[] ch=args[0].toCharArray();
+        for (int i=ch.length-1;i>=0;i--){
+            System.out.print(ch[i]);
+
+        }
+
+    }
+}
+
+
