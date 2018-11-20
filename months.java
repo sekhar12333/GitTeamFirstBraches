2080754ca79d4352c606ebe52fc75b786c8e8cb9
ChenReddyRaginiReddy <44599386+RaginiReddy02@users.noreply.github.com>
2018-11-20 01:33:04 -0800

    Add files via upload
    reserve n months programs

---
+++ months.java
@@ -0,0 +1,30 @@
+public class months {
+    public static void main(String[] args) {
+
+
+        switch (args[0]) {
+            case "1":
+                System.out.println("Monday");
+                break;
+            case "2":
+                System.out.println("tuesday");
+                break;
+            case "3":
+                System.out.println("wednesday");
+                break;
+            case "4":
+                System.out.println("thursday");
+                break;
+            case "5":
+                System.out.println("friday");
+                break;
+            case "6":
+                System.out.println("saturday");
+                break;
+            case "7":
+                System.out.println("sunday");
+                break;
+
+        }
+    }
+}
---
+++ reverse.java
@@ -0,0 +1,16 @@
+public class reverse {
+
+    public static void main(String[] args) {
+        String s= args[0];
+        char[] a=s.toCharArray();
+        for (int i=a.length-1; i>=0;i--){
+
+            System.out.println(a[i]);
+
+
+        }
+
+    }
+}
+
+
